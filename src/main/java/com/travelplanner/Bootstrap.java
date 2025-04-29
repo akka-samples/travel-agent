1d3c119bcefe0f12b6a79313ee618a51dabb2d89
Patrik Nordwall <patrik.nordwall@gmail.com>
2025-04-29 16:33:15 +0200

    chore: Better DI from bootstrap

--- src/main/java/com/travelplanner/Bootstrap.java
+++ src/main/java/com/travelplanner/Bootstrap.java
@@ -6,17 +6,22 @@ import akka.javasdk.ServiceSetup;
 import akka.javasdk.annotations.Setup;
 import akka.javasdk.client.ComponentClient;
 import com.travelplanner.application.TravelPlannerAgent;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+
 @Setup
 public class Bootstrap implements ServiceSetup {
+  final private static OpenAiChatModelName chatModelName = OpenAiChatModelName.GPT_4_O_MINI;
 
   private final Logger logger = LoggerFactory.getLogger(getClass());
-
-  private final TravelPlannerAgent travelPlannerAgent;
+  private final ComponentClient componentClient;
 
   public Bootstrap(ComponentClient componentClient) {
+    this.componentClient = componentClient;
     if (!KeyUtils.hasValidKeys()) {
       logger.error(
           "No API keys found. When running locally, make sure you have a " + ".env.local file located under " +
@@ -24,13 +29,20 @@ public class Bootstrap implements ServiceSetup {
               "make sure you have OPENAI_API_KEY defined as environment variable.");
       throw new RuntimeException("No API keys found.");
     }
-
-    this.travelPlannerAgent = new TravelPlannerAgent(componentClient);
   }
 
   @Override
   @SuppressWarnings("unchecked")
   public DependencyProvider createDependencyProvider() {
+    var chatModel = OpenAiChatModel.builder()
+        .modelName(chatModelName)
+        .apiKey(KeyUtils.readOpenAiKey())
+        .timeout(Duration.ofSeconds(60))
+        .logRequests(true)
+        .logResponses(true)
+        .build();
+    var travelPlannerAgent = new TravelPlannerAgent(componentClient, chatModel);
+
     return new DependencyProvider() {
       @Override
       public <T> T getDependency(Class<T> cls) {
